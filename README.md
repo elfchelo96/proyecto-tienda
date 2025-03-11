b1cbfe0cbf178ffad101767391b1cf5ccc64cde2
elfchelo96 <davidmarcelo.cr@gmail.com>
2025-03-11 19:35:52 -0400

    1.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# tienda
+# Ejemplo Tienda Vue.Js
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/modules/public/views/PublicLayout.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <div>
+        <h1>Public Layout</h1>
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
